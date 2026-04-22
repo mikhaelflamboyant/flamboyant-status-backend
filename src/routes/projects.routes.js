378edcb3a66e3eac8a2ffe9031b0297d65a3f381
Mikhael Maia - Tecnologia da Informação <mikhael.maia@flamboyant.com.br>
2026-04-22 16:27:09 -0300

    bug: correção na visualização dos projetos do fresh

--- src/routes/projects.routes.js
+++ src/routes/projects.routes.js
@@ -10,7 +10,7 @@ const authMiddleware = require('../middlewares/auth.middleware')
 
 router.use(authMiddleware)
 
-router.get('/freshservice-requests', requireRole(['ANALISTA_MASTER', 'COORDENADOR', 'GERENTE', 'SUPERINTENDENTE']), listFreshserviceRequests)
+router.get('/freshservice-requests', requireRole('ANALISTA_MASTER', 'COORDENADOR', 'GERENTE', 'SUPERINTENDENTE'), listFreshserviceRequests)
 router.get('/', listProjects)
 router.get('/archived', listArchivedProjects)
 router.get('/:id', getProjectById)
@@ -18,7 +18,7 @@ router.post('/', createProject)
 router.patch('/:id', updateProject)
 router.delete('/:id', deleteProject)
 router.post('/:id/members', assignMember)
-router.patch('/:id/approve-freshservice', requireRole(['ANALISTA_MASTER', 'COORDENADOR', 'GERENTE', 'SUPERINTENDENTE']), approveFreshservice)
-router.delete('/:id/reject-freshservice', requireRole(['ANALISTA_MASTER', 'COORDENADOR', 'GERENTE', 'SUPERINTENDENTE']), rejectFreshservice)
+router.patch('/:id/approve-freshservice', requireRole('ANALISTA_MASTER', 'COORDENADOR', 'GERENTE', 'SUPERINTENDENTE'), approveFreshservice)
+router.delete('/:id/reject-freshservice', requireRole('ANALISTA_MASTER', 'COORDENADOR', 'GERENTE', 'SUPERINTENDENTE'), rejectFreshservice)
 
 module.exports = router
