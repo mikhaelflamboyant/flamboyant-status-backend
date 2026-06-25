35a1d4ac488aeed6f01d00f3f154f0b86fee9a1a
Mikhael Maia - Tecnologia da Informação <mikhael.maia@flamboyant.com.br>
2026-06-25 17:23:40 -0300

    feat: notifications usa singleton prisma

--- src/services/notifications.service.js
+++ src/services/notifications.service.js
@@ -1,5 +1,4 @@
-const { PrismaClient } = require('@prisma/client')
-const prisma = new PrismaClient()
+const prisma = require('../lib/prisma')
 
 const createNotification = async ({ user_id, type, title, body, link }) => {
   return prisma.notification.create({
