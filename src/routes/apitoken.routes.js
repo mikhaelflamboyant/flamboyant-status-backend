14791ff173e740684aa24c8283ca376f10985c90
Mikhael Maia - Tecnologia da Informação <mikhael.maia@flamboyant.com.br>
2026-07-17 10:18:54 -0300

    feat: permissões do estagiário

--- src/routes/apitoken.routes.js
+++ src/routes/apitoken.routes.js
@@ -1,9 +1,11 @@
 const express = require('express')
 const router = express.Router()
 const authMiddleware = require('../middlewares/auth.middleware')
+const { requireRole } = require('../middlewares/role.middleware')
 const { listTokens, createToken, revokeToken, listAllTokens } = require('../controllers/apitoken.controller')
 
 router.use(authMiddleware)
+router.use(requireRole('ANALISTA_MASTER', 'ANALISTA_TESTADOR', 'SUPERINTENDENTE', 'DIRETOR', 'GERENTE', 'COORDENADOR', 'SUPERVISOR', 'ANALISTA'))
 router.get('/', listTokens)
 router.get('/history', listAllTokens)
 router.post('/', createToken)
